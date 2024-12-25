f7076072d209b967ceb058690d3c321345f7478a
will-hath <willhath@mit.edu>
2024-12-24 22:54:21 -0800

    fixed desk

---
+++ src/components/Desk/Desk.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Desk.css'
+
+const Desk = () => {
+    return (
+      <div className="desk-scene">
+        <div className="desk">
+          <div className="desk-front"></div>
+          <div className="desk-top"></div>
+        </div>
+      </div>
+    )
+};
+
+export default Desk;
