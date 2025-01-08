aa03bfd014368ebb22185afe136875ff760d0727
will-hath <willhath@mit.edu>
2025-01-07 22:14:01 -0800

    updated content

--- src/app/quotes/getContent.ts
+++ src/app/quotes/getContent.ts
@@ -1,38 +1,52 @@
 
 export function getquotesContent() {
   return `<blockquote>
-<p>&quot;Where there’s <strong>Will</strong> there <strong>Hath</strong> to be <strong>a way</strong>.&quot;
--- <em>Owen Boxer</em></p>
+<p>&quot;Alone, at the end of a universe humming a tune.&quot;<br>— <em>Unknown</em></p>
 </blockquote>
 <blockquote>
-<p>Alone, at the end of a universe humming a tune</p>
+<p>&quot;The whole idea that when you fall you gotta get up is bullshit. The whole thing is a fall. A perpetual state of grasping in the dark. It&#39;s not about getting up, it&#39;s about stumbling in the right direction. It&#39;s the only true way to move forward.&quot;<br>— <em>Unknown</em></p>
 </blockquote>
 <blockquote>
-<p>The whole idea that when you fall you gotta get up is bullshit. The whole thing is a fall. A perpetual state of grasping in the dark. It&#39;s not about getting up, it&#39;s about stumbling in the right direction. It&#39;s the only true way to move forward.</p>
+<p>&quot;It gets easier. Every day it gets a little easier. But you gotta do it every day—that’s the hard part. But it does get easier.&quot;<br>— <em>BoJack Horseman</em></p>
 </blockquote>
 <blockquote>
-<p>It gets easier. Every day it gets a little easier. But you gotta do it every day —that’s the hard part. But it does get easier</p>
-</blockquote>
-<p>The universe is a cruel, uncaring void. The key to being happy isn&#39;t a search for meaning. It&#39;s to just keep yourself busy with unimportant nonsense, and eventually, you&#39;ll be dead.</p>
-<p>There’s a star man, waiting in the sky
-He’d like to come and meet us but he thinks he’d blow our minds</p>
-<p>My God, a whole moment of happiness! Is that too little for the whole of a man&#39;s life?</p>
-<p>there is no name you could call me would make me value your approval</p>
-<p>There are no endings, and never will be endings, to the turning of the Wheel.
-But it was an ending. </p>
-<blockquote>
-<p>blessed be the peacemakers, for they will be shot at from two sides</p>
+<p>&quot;The universe is a cruel, uncaring void. The key to being happy isn&#39;t a search for meaning. It&#39;s to just keep yourself busy with unimportant nonsense, and eventually, you&#39;ll be dead.&quot;<br>— <em>Mr. Peanutbutter, BoJack Horseman</em></p>
 </blockquote>
 <blockquote>
-<p>there was truth and there was untruth, and if you clung to the truth, even against the whole world, you were not mad.</p>
+<p>&quot;There’s a starman waiting in the sky. He’d like to come and meet us, but he thinks he’d blow our minds.&quot;<br>— <em>David Bowie</em></p>
 </blockquote>
-<p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.</p>
-<p>Fling yourself straight into life, without deliberation; don’t be afraid - the flood will bear you back to	the bank and set you safe on your feet again</p>
-<p>above all, don’t lie to yourself.</p>
-<p>Quite simple.
-Be an example of humanity that is not disappointing</p>
-<p>We take risks, we know we take them.
-Therefore, when things come out against us, we have no cause for complaint.</p>
-<p>Everyone remembers icarus, nobody remembers the other guy</p>
+<blockquote>
+<p>&quot;My God, a whole moment of happiness! Is that too little for the whole of a man&#39;s life?&quot;<br>— <em>Fyodor Dostoevsky, White Nights</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;There is no name you could call me would make me value your approval.&quot;<br>— <em>Unknown</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;There are no endings, and never will be endings, to the turning of the Wheel. But it was an ending.&quot;<br>— <em>Robert Jordan, The Wheel of Time</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;Blessed be the peacemakers, for they will be shot at from two sides.&quot;<br>— <em>A. J. P. Taylor</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;There was truth and there was untruth, and if you clung to the truth, even against the whole world, you were not mad.&quot;<br>— <em>George Orwell, 1984</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.&quot;<br>— <em>John von Neumann</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;Fling yourself straight into life, without deliberation; don’t be afraid—the flood will bear you back to the bank and set you safe on your feet again.&quot;<br>— <em>Fyodor Dostoevsky, Demons</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;Above all, don’t lie to yourself.&quot;<br>— <em>Fyodor Dostoevsky, The Brothers Karamazov</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;Quite simple. Be an example of humanity that is not disappointing.&quot;<br>— <em>Unknown</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;We take risks, we know we take them. Therefore, when things come out against us, we have no cause for complaint.&quot;<br>— <em>Robert Falcon Scott</em></p>
+</blockquote>
+<blockquote>
+<p>&quot;Everyone remembers Icarus, nobody remembers the other guy.&quot;<br>— <em>Unknown</em></p>
+</blockquote>
 `;
 }
