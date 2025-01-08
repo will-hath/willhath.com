02d643b03417ed6b6668dcac1f7ff5d972d21312
will-hath <willhath@mit.edu>
2025-01-07 22:05:45 -0800

    uh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Desk from '@/components/Desk/Desk';
 import Image from 'next/image';
 
 const Tidbits: TVProps = {
-  imageSources: ["/assets/chalkboard.jpg"],
   textSources: [
     "The original DTP (Diptheria, Tetanus, Pertussis) vaccine, was considered too dangerous for use in the USA in the late 1980s due to neurological side-effects, and was replaced by DTaP, which uses acellular components of Pertussis, rather than whole-cell. However, DTP is still widely used in developing countries.",
     "There's a narrative in the USA that we have an obscene amount of national debt. But, if you compare our debt to GDP ratio to the rest of the world's 8 largest economies (China, UK, Italy, Japan, Germany, France, UK, India), ours is better than 2 of them, and is comparable to another 2.",
--- update_content.sh
+++ update_content.sh
@@ -4,8 +4,10 @@ cd ~/willhath.com
 # Run your commands
 /opt/homebrew/bin/node scripts/sync-tidbits.js
 /opt/homebrew/bin/node scripts/sync-about.js 
+/opt/homebrew/bin/node scripts/sync-quotes.js 
 git add src/app/tidbits/getContent.ts
 git add src/app/about/getContent.ts
+git add src/app/quotes/getContent.ts
 git commit -m "updated content"
 git push
 echo "Attempted to update content at $(date)"
