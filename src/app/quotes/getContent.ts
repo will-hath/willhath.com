b5005322b7e7675ea3efef505f908c6c17b73de7
will-hath <willhath@mit.edu>
2025-01-07 22:05:01 -0800

    updated content

--- src/app/quotes/getContent.ts
+++ src/app/quotes/getContent.ts
@@ -1,18 +1,31 @@
 
 export function getquotesContent() {
-  return `<p>Where there’s Will there <strong>hath</strong> to be <strong>a way</strong></p>
+  return `<blockquote>
+<p>Where there’s <strong>Will</strong> there <strong>Hath</strong> to be <strong>a way</strong>.</p>
+</blockquote>
+<p>Owen Boxer</p>
+<blockquote>
 <p>Alone, at the end of a universe humming a tune</p>
+</blockquote>
+<blockquote>
 <p>The whole idea that when you fall you gotta get up is bullshit. The whole thing is a fall. A perpetual state of grasping in the dark. It&#39;s not about getting up, it&#39;s about stumbling in the right direction. It&#39;s the only true way to move forward.</p>
+</blockquote>
+<blockquote>
 <p>It gets easier. Every day it gets a little easier. But you gotta do it every day —that’s the hard part. But it does get easier</p>
+</blockquote>
 <p>The universe is a cruel, uncaring void. The key to being happy isn&#39;t a search for meaning. It&#39;s to just keep yourself busy with unimportant nonsense, and eventually, you&#39;ll be dead.</p>
 <p>There’s a star man, waiting in the sky
 He’d like to come and meet us but he thinks he’d blow our minds</p>
 <p>My God, a whole moment of happiness! Is that too little for the whole of a man&#39;s life?</p>
 <p>there is no name you could call me would make me value your approval</p>
 <p>There are no endings, and never will be endings, to the turning of the Wheel.
-But it was an ending</p>
+But it was an ending. </p>
+<blockquote>
 <p>blessed be the peacemakers, for they will be shot at from two sides</p>
+</blockquote>
+<blockquote>
 <p>there was truth and there was untruth, and if you clung to the truth, even against the whole world, you were not mad.</p>
+</blockquote>
 <p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.</p>
 <p>Fling yourself straight into life, without deliberation; don’t be afraid - the flood will bear you back to	the bank and set you safe on your feet again</p>
 <p>above all, don’t lie to yourself.</p>
--- src/app/tidbits/getContent.ts
+++ src/app/tidbits/getContent.ts
@@ -1,15 +1,28 @@
 
 export function getTidbitsContent() {
-  return `<h4>Sunday, Dec 29, 2024</h4>
+  return `<h4>Sunday, Jan 5, 2025</h4>
+<p>Redwoods, despite the name, are not actually very red. Their bark looks a lot like cedar — in bright lighting, a light brown, interspersed with patches of a dilute burnt orange. However, if you take a knife and peel back the bark from a bough, you’ll notice a light red layer in between the brown bark and the white wood. </p>
+<h4>Saturday, Jan 4, 2025</h4>
+<p>Big Sur is a great place to go backpacking in in the winter. It’s 40-65 degrees and sunny (ymmv). </p>
+<h4>Friday, Jan 3, 2025</h4>
+<p>There is not a single Walmart store in San Francisco. </p>
+<h4>Thursday, Jan 2, 2025</h4>
+<p>In Legget (the town in Northern California where the 1 diverges from the 101), there is a redwood that you can drive your car through. Many decades ago, the people who owned the land cut an arch into it. The clearance is about 6’8”, and it costs $15. </p>
+<h4>Wednesday, Jan 1, 2025</h4>
+<p>The epic fantasy author Brandon Sanderson just released the 5th book in <em>The Stormlight Archive</em>, his gargantuan 10-book series (which he clearly hopes will cement his legacy as the greatest epic fantasy writer since Robert Jordan, if not Tolkien). However, he will not release the 6th book for at least 9 more years. This is because he’s planning on writing them all together before releasing any of them, ostensibly to make sure the narrative is as tight as possible. I think it’s fun to try to figure when famous artists are worrying about their legacies. </p>
+<h4>Sunday, Dec 29, 2024</h4>
 <p>Many roads in India (especially in the mountains) barely wide enough to support two-directional traffic. This poses a problem when the road goes around a turn, or if a portion of the road is washed out. Thus, in order to safely navigate corners, Indian drivers lay on their horns every time visibility is remotely obscured.</p>
 <h4>Saturday, Dec 28, 2024</h4>
-<p>The origins of the term &quot;twink&quot; are uncertain, but it most likely comes from the snack twinky. </p>
+<p>The origins of the term &quot;twink&quot; are uncertain, but it most likely comes from the hostess snack twinky. </p>
+<h4>Friday, Dec 27, 2024</h4>
 <h4>Thursday, Dec 26, 2024</h4>
-<p>Next JS&#39;s Image module gives you the ability to specify how your image will effect the way the page loads the first time someone opens it. For example, adding the &quot;priority&quot; tag blocks the page from rendering until that image has fully loaded and is ready to be displayed.</p>
+<p>Next JS&#39;s Image module gives you the ability to specify how your image will effect the way the page loads the first time someone opens it. For example, adding the &quot;priority&quot; tag blocks the rest of the page from being displayed to the user until that image has fully loaded and is ready to be displayed.</p>
 <h4>Monday, Dec 23, 2024</h4>
 <p>The original DTP (Diptheria, Tetanus, Pertussis) vaccine, was considered too dangerous for use in the USA in the late 1980s due to neurological side-effects, and was replaced by DTaP, which uses acellular components of Pertussis, rather than whole-cell. However, DTP is still widely used in developing countries. </p>
 <h4>Sunday, Dec 22, 2024</h4>
 <p>There&#39;s a narrative in the USA that we have an obscene amount of national debt. But, if you compare our debt to GDP ratio to the rest of the world&#39;s 8 largest economies (China, UK, Italy, Japan, Germany, France, UK, India), ours is better than 2 of them, and is comparable to another 2. </p>
+<h4>Saturday, Dec 21, 2024</h4>
+<p>California has 9 National Parks, the most of any state. I’ve been to 7 of them!</p>
 <h4>Friday, Dec 20, 2024</h4>
 <p>Jensen Huang went to Oregon State University. </p>
 <h4>Thursday, Dec 19</h4>
@@ -27,13 +40,13 @@ export function getTidbitsContent() {
 <h4>Saturday, Sep 28, 2024</h4>
 <p>The stability of consumer gasoline has deteriorated significantly since the 1980s, from ~9 months to ~2 months. This is largely due to the introduction of ethanol(modern gasoline contains ~10%), which absorbs water from the atmosphere over time, causing fuel to become less stable/efficient. Ethanol is introduced because it causes less greenhouse gas emissions, and it is able to be manufactured from agricultural products (corn, sugarcane, etc) rather than drilled for like oil. </p>
 <h4>Sunday, Jul 14, 2024</h4>
-<p><em>San Francisco (Be Sure to Wear Some Flowers in your Hair)</em>, by Scott McKenzie, was the anthem of the summer of love, and actually drove a bunch of people to come to San Francisco (rather than the correlation going the other way as I&#39;d assumed)</p>
+<p><em>San Francisco (Be Sure to Wear Some Flowers in your Hair)</em>, by Scott McKenzie, was the anthem of the summer of love, and actually drove a bunch of people to come to San Francisco (rather than the correlation going the other way as I&#39;d assumed). </p>
 <h4>Saturday, Jul 13, 2024</h4>
 <p>Trumpets seem complicated to play. There are two ways to alter the pitch -- with your mouth, and with the valves. Your mouth gives most of the range, but because of how tubes work, you&#39;re only able to hit the harmonics, which decrease in spacing as they increase. So, for a &quot;C&quot; trumpet, you can (in order) play a low C, a middle C, a middle G, a high C, a high E, a high A flat, etc. If you want to hit the notes in between, you have to use the valves, which you control with your fingers. The effectively change the length of the trumpet tube by a fixed amount, which each correspond to a fixed decrease in pitch -- a half tone, a whole tone, or a minor third. By pressing them in combination, you can hit 8 ($2^{3}$) unique notes for each harmonic. </p>
 <h4>Thursday, Jul 11, 2024</h4>
 <p>If the moon lost all it&#39;s momentum (relative to the earth), it would take about two weeks until it crashed into us. </p>
 <h4>Monday, Jul 1, 2024</h4>
-<p>When estimating the net worth of individuals, Forbes values stock held in private companies at 90 cents on the dollar. So if you&#39;re a founder who owns 10% of a company that&#39;s worth 10 billion, you&#39;re &quot;worth&quot; 900 million.</p>
+<p>When estimating the net worth of individuals, Forbes values stock held in private companies at 90 cents on the dollar. So if you own 10% of a company that&#39;s worth 10 billion, you&#39;re &quot;worth&quot; 900 million.</p>
 <h4>Monday, Jun 10, 2024</h4>
 <p>OpenAI&#39;s model spec has nothing explicitly against racism, other than to encourage kindness and fairness while discouraging hate.</p>
 <h4>Friday, May 31, 2024</h4>
@@ -50,9 +63,9 @@ $$</p>
 <h4>Friday, Feb 29, 2024</h4>
 <p>The first primary use of Galileo’s discovery of the jovian moons was to redraw all of our maps! Since the Jovian eclipses happen very frequently, you can compare your local time to your global time (based off when the eclipse happened). </p>
 <h4>Sunday, Feb 11, 2024</h4>
-<p>Apparently MIT offers it&#39;s students an interest free 5000 dollar loan. If every freshman at MIT took it out and put it into treasury bonds, they could get a free 1000 dollars when they graduate.</p>
+<p>Apparently MIT offers each of it&#39;s students an 4 year, interest free 5000 dollar loan. If every freshman at MIT took it out and put it into treasury bonds, they could get a free 1000 dollars when they graduate.</p>
 <h4>Saturday, Feb 10, 2024</h4>
-<p>You can automatically switch the image type of an image on mac by going &quot;right click-&gt;quick actions-convert image&quot; in finder.</p>
+<p>You can automatically switch the image type of an image on mac by going &quot;right click-&gt;quick actions-&gt;convert image&quot; in finder.</p>
 <h4>Monday, Feb 5, 2024</h4>
 <p>You can add /students to the url in canvas to see everyone that&#39;s registered for a class.</p>
 <h4>Monday, Jan 29, 2024</h4>
@@ -72,7 +85,7 @@ $$</p>
 <h4>Saturday, Jan 20, 2024</h4>
 <p>The building manager of The Stud (student center at MIT) is named Tim Woods and he can be pretty reasonable once you get to know him.</p>
 <h4>Friday, Jan 19, 2024</h4>
-<p>If you have a vertical plank of wood holding back water (ie, in a rectangular tank), the center of torque is at 1/3 the height of the water.</p>
+<p>If you have a vertical wall holding back water (ie, in a rectangular tank), the center of torque is at 1/3 the height of the water.</p>
 <h4>Wednesday, Jan 10, 2024</h4>
 <p>In economics, there’s this thing called the “productivity J curve”, where basically, after a new technology is discovered, “productivity” (as measured via GDP) will stagnate, because free capitol is being invested into the new technology (ie, building railroad lines, integrating AI infrastructure, etc). But then, once these shorter-term investments pay off, GPD rises faster again, overcoming the rate of decreased productivity. </p>
 <h4>Tuesday, Jan 9, 2024</h4>
@@ -80,11 +93,11 @@ $$</p>
 <h4>Monday, Jan 8, 2024</h4>
 <p>Apparently Oxford has a secret “night climbers” club where people climb the walls + towers + roofs around Oxford. It seems to have a bigger focus on athleticism than MIT&#39;s.</p>
 <h4>Sunday, Jan 7, 2024</h4>
-<p>The Estonian flag looks like the average landscape in Estonia: White snow, black trees, blue sky. Look it up, you’ll see what I mean!</p>
+<p>The Estonian flag looks like the average landscape in Estonia: White snow, black trees, blue sky. Look it up, you’ll see what I mean. </p>
 <h4>Saturday, Jan 6, 2024</h4>
 <p>The ideal strategy in Ticket to Ride is probably to hoard cards at the beginning of the game (higher locomotives/card average, and you preserve optionality as long as possible). Also, start with game with fairly few tickets, and make sure they all can be linked together. People usually undervalue the utility of just building lots of roads and going out of trains. Also, in the EU version, Palermo to Moscow is the best long-route since that part of the board is much less crowded.</p>
 <h4>Friday, Jan 5, 2024</h4>
-<p>Apparently most countries other than the US have interest free investment accounts with <strong>no</strong> restrictions on what they can be used for (Canada has TFSA, UK has ISA). The limits are higher or comparable to in the US. The US has Roth IRA, but that has super strict limits on how you can withdraw from it, usually is just for retirement savings. </p>
+<p>Apparently most countries other than the USA have interest free investment accounts with <strong>no</strong> restrictions on what they can be used for (Canada has TFSA, UK has ISA). The limits are higher or comparable to in the USA. The USA has the Roth IRA, but that has strict limits on how you can withdraw from it, and is intended just for retirement. </p>
 <h4>Thursday, Jan, 4 2024</h4>
 <p>Estonia, Finland, and maybe a few other countries are all in the same free-market energy pricing scheme. Due to extremely cold temperatures as well as less supply (I assume Russia/Ukraine stuff), energy prices are now such that running a small space heater for one day would cost ~€150. God bless America, and her oil. </p>
 `;
