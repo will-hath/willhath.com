f40dcd40ca287329fe8504cf66994333480e51cc
will-hath <willhath@mit.edu>
2025-01-14 07:57:20 -0800

    added photo to gallery

--- src/constants/photoGallery.ts
+++ src/constants/photoGallery.ts
@@ -1,1 +1,1 @@
-export const photoGallery = Array.from({ length: 34 }, (_, index) => `/assets/gallery/${index}.jpg`);
+export const photoGallery = Array.from({ length: 35 }, (_, index) => `/assets/gallery/${index}.jpg`);
