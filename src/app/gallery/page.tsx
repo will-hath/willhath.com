e08e169bf01b10fe069816e75b03181b48545bc5
will-hath <willhath@mit.edu>
2025-01-24 18:35:14 -0800

    improved gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,37 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./gallery.css";
-import { photoGallery } from '@/constants/photoGallery';
+import { photoGallery } from "@/constants/photoGallery";
 
 export default function GalleryPage() {
-  // Create an array of image paths
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+  const openModal = (index: number) => {
+    setSelectedIndex(index);
+  };
+
+  const closeModal = () => {
+    setSelectedIndex(null);
+  };
+
+  const showPrev = (e: React.MouseEvent) => {
+    // prevent the click from also triggering closeModal
+    e.stopPropagation();
+    if (selectedIndex !== null) {
+      setSelectedIndex((prevIndex) =>
+        prevIndex === 0 ? photoGallery.length - 1 : (prevIndex as number) - 1
+      );
+    }
+  };
+
+  const showNext = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (selectedIndex !== null) {
+      setSelectedIndex((prevIndex) =>
+        prevIndex === photoGallery.length - 1 ? 0 : (prevIndex as number) + 1
+      );
+    }
+  };
 
   return (
     <main className="gallery-container">
@@ -12,10 +39,31 @@ export default function GalleryPage() {
       <div className="gallery-grid">
         {photoGallery.map((src, i) => (
           <div key={i} className="gallery-item">
-            <img src={src} alt={`Gallery item ${i}`} />
+            <img
+              src={src}
+              alt={`Gallery item ${i}`}
+              onClick={() => openModal(i)}
+            />
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      {selectedIndex !== null && (
+        <div className="gallery-modal" onClick={closeModal}>
+          <button className="nav-button prev-button" onClick={showPrev}>
+            &larr;
+          </button>
+          <img
+            className="modal-image"
+            src={photoGallery[selectedIndex]}
+            alt={`Modal item ${selectedIndex}`}
+          />
+          <button className="nav-button next-button" onClick={showNext}>
+            &rarr;
+          </button>
+        </div>
+      )}
     </main>
   );
 }
