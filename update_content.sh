a94847507d9d0c30da0e41b52966bb38e18c5125
will-hath <willhath@mit.edu>
2025-08-25 02:21:27 -0400

    wip

--- update_content.sh
+++ update_content.sh
@@ -1,11 +1,10 @@
 # Navigate to the directory
 cd ~/willhath.com
 
-# Run your commands
+
 /opt/homebrew/bin/node scripts/sync-tidbits.js
 /opt/homebrew/bin/node scripts/sync-about.js 
 /opt/homebrew/bin/node scripts/sync-quotes.js 
-git add src/app/tidbits/getContent.ts
 git add src/app/tidbits/tidbitsArray.ts
 git add src/app/about/getContent.ts
 git add src/app/quotes/getContent.ts
