19c7ba721f530121a7bb22c70124a1cf83b9ac25
will-hath <willhath@mit.edu>
2025-10-28 22:15:48 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Disable sharp to avoid simple-swizzle dependency
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.html$/,
